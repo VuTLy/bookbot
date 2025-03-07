7b03fe6e405b8c1bdcb8a6219ed9e2f2fa257eda
VuTLy <vulyccschool@gmail.com>
2025-03-06 20:28:09 -0800

    add commandline argument to pass in path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import count_words, appearence, most_common
+import sys
 
+#Open file and read in content
 def get_book_text(file):
     with open(file) as f:
         return f.read()
@@ -7,7 +9,13 @@ def get_book_text(file):
 
 def main():
 
-    path = "books/frankenstein.txt"
+    #create commandline argument to pass in path 
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1]
+
     contents = get_book_text(path)
     num_words = count_words(contents)
     appear = appearence(contents)
@@ -18,7 +26,7 @@ def main():
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for count in most:
+    for count in most: #we format using the two key in the nested dict of char and the count
         print(f"{count['char']}: {count['count']}")    
     print("============= END ===============")
 
