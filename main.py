f4d536265431650126ff7b4a5db6a2738c82737f
VuTLy <vulyccschool@gmail.com>
2025-03-06 19:31:13 -0800

    Accessing content, refactor word count, ignore __pycache__/

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import count_words
+
+def get_book_text(file):
+    with open(file) as f:
+        return f.read()
+    
+
+def main():
+
+    path = "books/frankenstein.txt"
+    contents = get_book_text(path)
+    num_words = count_words(contents)
+    print(f"{num_words} words found in the document")
+
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text):
+    word = text.split()
+    return len(word)
